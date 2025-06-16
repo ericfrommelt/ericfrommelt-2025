2380a00854653e8c19763861cfdc5516b5010550
ericfrommelt <eric@alphanumeric.io>
2025-06-16 12:16:30 -0700

    copy update

--- src/app/info/page.js
+++ src/app/info/page.js
@@ -13,7 +13,7 @@ export default function Info() {
           />
         </div>
         <div className="col-span-1">
-          <h1 className="text-5xl font-light py-10">3D art + design. Human creativity.</h1>
+          <h1 className="text-5xl font-light py-10">Digital art + design. Human creativity.</h1>
           <p className="mt-2">I&apos;m a digital artist and designer based in Los Angeles, CA. I partner with brands on visual exploration and storytelling through design.</p>
           <p className="mt-2">I love light, running, and music.</p>
           <p className="mt-20">Let&apos;s work together<br /><a href="mailto:eric@ericfrommelt.com" className="hover:underline">eric@ericfrommelt.com</a></p>
